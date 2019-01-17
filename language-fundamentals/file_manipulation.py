8ff9cf017fbfa4abc832abcdc8d30837627fb641
Adam Chidgey <40761319+chidgey@users.noreply.github.com>
2019-01-17 20:49:31 +0000

    File names changed, code needed updating

--- language-fundamentals/file_manipulation.py
+++ language-fundamentals/file_manipulation.py
@@ -3,7 +3,7 @@
 """Manipulating files on disk with simple text file manipulation."""
 
 # make sure you are in the right directory in the terminal before running this.
-myfile =  open('file-manipulation-read.txt')
+myfile =  open('file_manipulation_read.txt')
 
 # read the file content, from the beginning to end
 fileContent = myfile.read()
@@ -24,27 +24,27 @@ print(fileContent)
 myfile.close()
 
 # this is a better way to use and close the file resource
-with open('file-manipulation-read.txt', mode='r') as my_new_file:
+with open('file_manipulation_read.txt', mode='r') as my_new_file:
     contents = my_new_file.read()
 
 print(contents)
 
 # writing to a new file
-with open('file-manipulation-write.txt', mode='w') as myfile:
-    contents = myfile.write('Welcome to the new file! Please refer to file-manipulation.py')
+with open('file_manipulation_write.txt', mode='w') as myfile:
+    contents = myfile.write('Welcome to the new file! Please refer to file_manipulation.py')
     # myfile.read() this will throw an error
 
 # append only - good for logs
-with open('file-manipulation-write.txt', mode='a') as myfile:
+with open('file_manipulation_write.txt', mode='a') as myfile:
     contents = myfile.write('\nTHIS WAS APPENDED')
     # myfile.read() this will throw an error
 
 # reading and writing to an existing file
-with open('file-manipulation-write.txt', mode='r+') as myfile:
+with open('file_manipulation_write.txt', mode='r+') as myfile:
     contents = myfile.write('...OVERWRITTEN...')
     myfile.read() # all good we can read too.
 
 # writing a new file and allows reading
-with open('file-manipulation-overwrite.txt', mode='w+') as myfile:
+with open('file_manipulation_overwrite.txt', mode='w+') as myfile:
     contents = myfile.write('overwrites existing and allows reading too')
     myfile.read() # all good we can read too.
